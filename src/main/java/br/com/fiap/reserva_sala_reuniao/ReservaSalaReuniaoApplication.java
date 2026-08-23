4a20ee4808e8b18accf244d520ad001e49d91e56
Joao Antonio de Lima Passone <joao.passone@gmail.com>
2026-08-23 19:03:03 -0300

    Commit Inicial

---
+++ src/main/java/br/com/fiap/reserva_sala_reuniao/ReservaSalaReuniaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.reserva_sala_reuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSalaReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSalaReuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/reserva_sala_reuniao/ReservaSalaReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.reserva_sala_reuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSalaReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
